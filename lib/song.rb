818d1239d3ae42f16d230d3dfedd69279eb2534a
Sara Isabel Garcia Reyes <sarisgar28@gmail.com>
2020-03-27 15:40:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,7 +31,7 @@ class Song
   def self.genre_count 
     genres_hash = { } 
     @@genres.each do |genre|
-      if genres_hash[genre] 
+      if genres_hash[genre] == 0 
         genres_hash[genre] = 1
       else 
         genres_hash[genre] += 1 
