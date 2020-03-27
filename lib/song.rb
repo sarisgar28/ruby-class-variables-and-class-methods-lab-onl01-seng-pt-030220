fc3c2546cb6dde9d8168d40f6647325a175d0234
Sara Isabel Garcia Reyes <sarisgar28@gmail.com>
2020-03-27 15:27:20 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,44 @@
+
+class Song 
+  
+  attr_accessor :name, :artist, :genre 
+  
+  @@count = 0 
+  @@genres = [ ]
+  @@artists = [ ]
+  
+  def initialize(name,artist,genre)
+    @@count += 1 
+    @name = name 
+    @artist = artist 
+    @genre = genre 
+    @@genres << genre
+    @@artists << artist
+  end 
+  
+ def self.count
+   @@count 
+ end 
+ 
+  def self.artists
+    @@artists.uniq 
+   end 
+   
+   def self.genres 
+     @@genres.uniq
+   end 
+   
+  def self.genre_count 
+    genres_hash = { } 
+    @@genres.each do |genre|
+      if genres_hash[genre] = 1 
+      else 
+        genres_hash[genre] += 1 
+      end 
+    end
+ end 
+ 
+end 
+
+ 
+ 
