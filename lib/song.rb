0357ec0bc0543b1b456651ae8985e036c4c3fdc2
Sara Isabel Garcia Reyes <sarisgar28@gmail.com>
2020-03-27 15:39:13 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -32,6 +32,7 @@ class Song
     genres_hash = { } 
     @@genres.each do |genre|
       if genres_hash[genre] = 1
+        genres_hash[genre] = 1
       else 
         genres_hash[genre] += 1 
       end 
